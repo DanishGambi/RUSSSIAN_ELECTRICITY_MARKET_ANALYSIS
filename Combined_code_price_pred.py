e8f8a16500607b4e552d816928871ec6a21329f3
DanishGambi <danish_gambi@yahoo.com>
2025-12-22 19:25:13 +1000

    Update Combined_code_price_pred.py

--- Combined_code_price_pred.py
+++ Combined_code_price_pred.py
@@ -1,3 +1,22 @@
+# -------------------------------------------------------------------
+# PROJECT: Анализ потребления электроэнергии в регионах России
+# -------------------------------------------------------------------
+# Copyright [2024] [Dmitrii A. Maliuzhantsev, Arina M. Tarasova,
+#                   Anna K. Andaralova, Polina S. Belokorovii,
+#                   Dariana A. Salchak, Aleksandr Yu. Filatov]
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# -------------------------------------------------------------------
 import matplotlib
 #matplotlib.use('Agg')
 from matplotlib import pyplot as plt
@@ -172,3 +191,4 @@ Summary_plot(shap_values, X, file_name[:-4])
 Bar_plot(shap_values, X, file_name[:-4])
 for num in range (0, 40):
     Dependence_plot(shap_values, X, file_name[:-4], num)
+
