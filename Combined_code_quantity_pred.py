ac57ff3dba26ebf9a0cdc54226d3a1d06cbbbcac
DanishGambi <danish_gambi@yahoo.com>
2025-12-22 19:29:11 +1000

    Update Combined_code_quantity_pred.py

--- Combined_code_quantity_pred.py
+++ Combined_code_quantity_pred.py
@@ -1,9 +1,7 @@
 # -------------------------------------------------------------------
 # PROJECT: Анализ потребления электроэнергии в регионах России
 # -------------------------------------------------------------------
-# Copyright [2024] [Dmitrii A. Maliuzhantsev, Arina M. Tarasova,
-#                   Anna K. Andaralova, Polina S. Belokorovii,
-#                   Dariana A. Salchak, Aleksandr Yu. Filatov]
+# Copyright 2024 Dmitrii A. Maliuzhantsev
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -191,4 +189,5 @@ Summary_plot(shap_values, X, file_name[:-4])
 Bar_plot(shap_values, X, file_name[:-4])
 for num in range (0, 40):
     Dependence_plot(shap_values, X, file_name[:-4], num)
+
 
