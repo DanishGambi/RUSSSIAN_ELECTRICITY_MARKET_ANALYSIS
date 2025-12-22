24da26e2f0cd79e80b35fcaa74a6bf62d9c5b077
DanishGambi <danish_gambi@yahoo.com>
2025-12-22 19:25:51 +1000

    Update Irkutsk_model_parameter_tuning.py

--- Irkutsk_model_parameter_tuning.py
+++ Irkutsk_model_parameter_tuning.py
@@ -1,3 +1,22 @@
+# -------------------------------------------------------------------
+# PROJECT: Анализ потребления электроэнергии в регионах России
+# -------------------------------------------------------------------
+# Copyright [2024] [Dmitrii A. Maliuzhantsev, Arina M. Tarasova,
+#                   Anna K. Andaralova, Polina S. Belokorovii,
+#                   Dariana A. Salchak, Aleksandr Yu. Filatov]
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# -------------------------------------------------------------------
 from matplotlib import pyplot as plt
 from sklearn.ensemble import HistGradientBoostingRegressor
 from sklearn.model_selection import train_test_split, GridSearchCV
@@ -77,3 +96,4 @@ print("Best Cross-Validated MSE Score:", best_score)
 # Optional: Fit the best model on full data for future predictions
 best_model = grid_search.best_estimator_
 best_model.fit(X, y)
+
